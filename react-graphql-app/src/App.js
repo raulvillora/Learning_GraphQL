6d4f57aa5d37a4b9b164da2f201f2a2eb03bd438
Raul Villora Valencia <r.vvalencia@alumnos.upm.es>
2019-03-05 18:25:54 +0100

    Added classes methods

--- react-graphql-app/src/App.js
+++ react-graphql-app/src/App.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
 
-const axiosGitHub = axios.create({
-  baseURL: 'https://api.github.com/graphql',
-  headers: {
-    Authorization: 'bearer YOUR_GITHUB_PERSONAL_ACCESS_TOKEN',
-  },
-});
+// const axiosGitHub = axios.create({
+// 	baseURL: 'https://api.github.com/graphql',
+// 	headers: {
+// 		Authorization: 'bearer YOUR_GITHUB_PERSONAL_ACCESS_TOKEN'
+// 	}
+// });
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.onSubmit}>
-          <label htmlFor="url">
-            Showing open issues for https://github.com/raulvillora
-          </label>
-          <input id="url"
-            type="text"
-            onChange={this.onChange}
-            style={{ width: '330px' }}/>
-            <button type="submit">Search</button>
-        </form>
-        <hr />
-      </div>
-    );
-  }
+
+  state = {
+    path: '',//Add path to search
+  };
+  
+	componentDidMount() {
+		//fetch data
+	}
+
+	onChange = (event) => {
+		this.setState({
+			path: event.target.value
+		});
+  };
+  
+  onSubmit = (event) => {
+    //fetch data
+    event.preventDefault();
+  };
+
+	render() {
+    const{ path } = this.state;
+		return (
+			<div>
+				<form onSubmit={this.onSubmit}>
+					<label htmlFor="url" style={{fontFamily: "Avenir", paddingTop: "3vh"}}>Showing open issues for https://github.com/raulvillora/ </label>
+					<input id="url" type="text" onChange={this.onChange} value={ path } style={{ width: '330px' }} />
+					<button type="submit">Search</button>
+				</form>
+				<hr />
+			</div>
+		);
+	}
 }
